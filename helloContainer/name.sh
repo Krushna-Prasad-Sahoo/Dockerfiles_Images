dbf43bcd2b0070f0d2f57e24e5361c0c3b1c3d5c
Krushna Prasad Sahoo <krushna.360degree@gmail.com>
2022-03-04 04:46:15 +0530

    Update name.sh

--- helloContainer/name.sh
+++ helloContainer/name.sh
@@ -8,3 +8,11 @@ echo "Hello $@ !!!!"
 sleep 2
 echo "Your container is successfully launched .."
 sleep 2
+echo "Now shutting down  ... BYE BYE !!"
+sleep 2
+echo
+tput smul
+echo "This Image is createds by KP :)"
+sleep 2
+tput sgr0
+echo
